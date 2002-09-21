7a758fa6b2d1c7714dcbd802072c1a7dd1215d78
edwards <edwards>
2002-09-21 16:16:13 +0000

    Cosmetic changes. Moved some of the scalar stuff to specializations and out of defs.

--- defs.h
+++ defs.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: defs.h,v 1.1 2002-09-12 18:22:16 edwards Exp $
+// $Id: defs.h,v 1.2 2002-09-21 16:16:13 edwards Exp $
 //
 // QDP data parallel interface
 //
@@ -15,7 +15,6 @@ typedef GammaType<Ns> Gamma;
 
 // Aliases for a scalar architecture
 typedef OLattice< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > LatticeGauge;
-//typedef OLattice< PScalar< PMatrix< RComplex<REAL32>, Nc> > > LatticeGauge;
 typedef OLattice< PSpinVector< PColorVector< RComplex<REAL32>, Nc>, Ns> > LatticeFermion;
 typedef OLattice< PSpinVector< PColorVector< RComplex<REAL32>, Nc>, Ns>>1 > > LatticeHalfFermion;
 typedef OLattice< PSpinMatrix< PColorMatrix< RComplex<REAL32>, Nc>, Ns> > LatticePropagator;
@@ -34,14 +33,6 @@ typedef OScalar< PSpinVector< PColorVector< RComplex<REAL32>, Nc>, Ns> > Fermion
 typedef OScalar< PSpinMatrix< PColorMatrix< RComplex<REAL32>, Nc>, Ns> > Propagator;
 typedef OScalar< PScalar< PScalar< RComplex<REAL32> > > > Complex;
 
-  // Duplicate names
-typedef OScalar< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > ColorMatrix;
-typedef OScalar< PScalar< PColorVector< RComplex<REAL32>, Nc> > > ColorVector;
-typedef OScalar< PSpinMatrix< PScalar< RComplex<REAL32> >, Ns> > SpinMatrix;
-typedef OScalar< PSpinVector< PScalar< RComplex<REAL32> >, Ns> > SpinVector;
-typedef OScalar< PSpinVector< PColorVector< RComplex<REAL32>, Nc>, Ns> > ColorVectorSpinVector;
-typedef OScalar< PSpinMatrix< PColorMatrix< RComplex<REAL32>, Nc>, Ns> > ColorMatrixSpinMatrix;
-
 typedef OScalar< PScalar< PSeed< RScalar<INTEGER32> > > > Seed;
 typedef OScalar< PScalar< PScalar< RScalar<INTEGER32> > > > Integer;
 typedef OScalar< PScalar< PScalar< RScalar<REAL32> > > > Real;
@@ -51,102 +42,19 @@ typedef OScalar< PScalar< PScalar< RScalar<LOGICAL> > > > Boolean;
 typedef OScalar< PScalar< PScalar< RComplex<REAL64> > > > DComplex;
 
 
+// Duplicate names
+typedef OScalar< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > ColorMatrix;
+typedef OScalar< PScalar< PColorVector< RComplex<REAL32>, Nc> > > ColorVector;
+typedef OScalar< PSpinMatrix< PScalar< RComplex<REAL32> >, Ns> > SpinMatrix;
+typedef OScalar< PSpinVector< PScalar< RComplex<REAL32> >, Ns> > SpinVector;
+typedef OScalar< PSpinVector< PColorVector< RComplex<REAL32>, Nc>, Ns> > ColorVectorSpinVector;
+typedef OScalar< PSpinMatrix< PColorMatrix< RComplex<REAL32>, Nc>, Ns> > ColorMatrixSpinMatrix;
+
+// Level below outer for internal convenience
 typedef PScalar< PScalar< RScalar<REAL32> > > IntReal;
 typedef PScalar< PScalar< RScalar<INTEGER32> > > IntInteger;
 typedef PScalar< PScalar< RScalar<REAL64> > > IntDouble;
 typedef PScalar< PScalar< RScalar<LOGICAL> > > IntBoolean;
-
-
-// Construct simple float word
-template<>
-struct SimpleScalar<float>
-{
-  typedef Real   Type_t;
-};
-
-// Construct simple float word
-template<>
-struct SimpleScalar<int>
-{
-  typedef Integer   Type_t;
-};
-
-// Construct simple float word
-template<>
-struct SimpleScalar<double>
-{
-  typedef Double   Type_t;
-};
-
-// Construct simple boolean word
-template<>
-struct SimpleScalar<bool>
-{
-  typedef Boolean   Type_t;
-};
-
-
-// Construct simple float word
-template<>
-struct InternalScalar<float>
-{
-  typedef float  Type_t;
-};
-
-// Construct simple int word
-template<>
-struct InternalScalar<int>
-{
-  typedef int   Type_t;
-};
-
-// Construct simple double word
-template<>
-struct InternalScalar<double>
-{
-  typedef double   Type_t;
-};
-
-// Construct simple boolean word
-template<>
-struct InternalScalar<bool>
-{
-  typedef bool  Type_t;
-};
-
-
-
-struct CreateLeaf<int>
-{
-  typedef int Inp_t;
-  typedef Integer  Leaf_t;
-  inline static
-  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
-};
-
-struct CreateLeaf<float>
-{
-  typedef float Inp_t;
-  typedef Real  Leaf_t;
-  inline static
-  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
-};
-
-struct CreateLeaf<double>
-{
-  typedef double Inp_t;
-  typedef Double  Leaf_t;
-  inline static
-  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
-};
-
-struct CreateLeaf<bool>
-{
-  typedef bool Inp_t;
-  typedef Boolean  Leaf_t;
-  inline static
-  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
-};
 
 
 QDP_END_NAMESPACE();
--- specializations.h
+++ specializations.h
@@ -1,10 +1,113 @@
 // -*- C++ -*-
-// $Id: specializations.h,v 1.1 2002-09-12 18:22:16 edwards Exp $
+// $Id: specializations.h,v 1.2 2002-09-21 16:16:13 edwards Exp $
 //
 // QDP data parallel interface
 //
 
 QDP_BEGIN_NAMESPACE(QDP);
+
+//
+// Return an equivalent QDP type given some simple machine type
+//
+template<>
+struct SimpleScalar<float>
+{
+  typedef Real   Type_t;
+};
+
+// Construct simple float word
+template<>
+struct SimpleScalar<int>
+{
+  typedef Integer   Type_t;
+};
+
+// Construct simple double word
+template<>
+struct SimpleScalar<double>
+{
+  typedef Double   Type_t;
+};
+
+// Construct simple boolean word
+template<>
+struct SimpleScalar<bool>
+{
+  typedef Boolean   Type_t;
+};
+
+
+//
+// Type constructors for QDP types within the type system. Namely,
+// at some level like a primitive, sometimes scalar temporaries are needed
+// These are the bottom most constructors given a machine type
+//
+template<>
+struct InternalScalar<float>
+{
+  typedef float  Type_t;
+};
+
+// Construct simple int word
+template<>
+struct InternalScalar<int>
+{
+  typedef int   Type_t;
+};
+
+// Construct simple double word
+template<>
+struct InternalScalar<double>
+{
+  typedef double   Type_t;
+};
+
+// Construct simple boolean word
+template<>
+struct InternalScalar<bool>
+{
+  typedef bool  Type_t;
+};
+
+
+
+//
+// Leaf constructors for simple machine types. These are a specialization over the
+// default constructors. The point is to avoid wrapping the simple types in
+// references which do not help much (primitive objects are word size!), 
+// but get in the way of type computations.
+//
+struct CreateLeaf<int>
+{
+  typedef int Inp_t;
+  typedef Integer  Leaf_t;
+  inline static
+  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
+};
+
+struct CreateLeaf<float>
+{
+  typedef float Inp_t;
+  typedef Real  Leaf_t;
+  inline static
+  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
+};
+
+struct CreateLeaf<double>
+{
+  typedef double Inp_t;
+  typedef Double  Leaf_t;
+  inline static
+  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
+};
+
+struct CreateLeaf<bool>
+{
+  typedef bool Inp_t;
+  typedef Boolean  Leaf_t;
+  inline static
+  Leaf_t make(const Inp_t &a) { return Leaf_t(a); }
+};
 
 
 template<>
