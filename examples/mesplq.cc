1e9ca83266434cf35ceb14d8aec22a593d354a43
edwards <edwards>
2002-09-24 02:03:53 +0000

    Small code cleanup.

--- examples/mesplq.cc
+++ examples/mesplq.cc
@@ -1,4 +1,4 @@
-// $Id: mesplq.cc,v 1.2 2002-09-14 19:48:46 edwards Exp $
+// $Id: mesplq.cc,v 1.3 2002-09-24 02:03:53 edwards Exp $
 //
 //! Return the value of the average plaquette normalized to 1
 /*!
@@ -11,12 +11,6 @@
 #include "tests.h"
 
 using namespace QDP;
-
-LatticeReal fred(const LatticeReal& v)
-{
-  return v;
-}
-
 
 void MesPlq(const multi1d<LatticeGauge>& u, Double& w_plaq, Double& s_plaq, 
 	    Double& t_plaq, Double& link)
@@ -41,8 +35,8 @@ void MesPlq(const multi1d<LatticeGauge>& u, Double& w_plaq, Double& s_plaq,
       /* tmp_0 = u(x+mu,nu)*u_dag(x+nu,mu) */
       /* tmp_1 = tmp_0*u_dag(x,nu)=u(x+mu,nu)*u_dag(x+nu,mu)*u_dag(x,nu) */
       /* wplaq_tmp = tr(u(x,mu)*tmp_1=u(x,mu)*u(x+mu,nu)*u_dag(x+nu,mu)*u_dag(x,nu)) */
-      Double tmp = real(innerproduct(u[nu],
-				     u[mu]*(shift(u[nu],FORWARD,mu)*conj(shift(u[mu],FORWARD,nu)))));
+      Double tmp = real(
+	innerproduct(u[nu],u[mu]*(shift(u[nu],FORWARD,mu)*conj(shift(u[mu],FORWARD,nu)))));
 #endif
       w_plaq += tmp;
 
@@ -58,18 +52,10 @@ void MesPlq(const multi1d<LatticeGauge>& u, Double& w_plaq, Double& s_plaq,
   if (Nd > 2) 
     s_plaq *= 2.0 / double(geom.Vol()*(Nd-1)*(Nd-2)*Nc);
   
-  t_plaq = t_plaq / double(geom.Vol()*(Nd-1)*Nc);
+  t_plaq /= double(geom.Vol()*(Nd-1)*Nc);
   
   for(int mu=0; mu < Nd; ++mu)
-  { 
-//    LatticeReal link_tmp = trace_real(u[mu]);
-//    link += sum(link_tmp);
-
-//    link += sum(trace_real(u[mu]));
     link += sum(LatticeReal(real(trace(u[mu]))));
 
-//    link += sum(fred(trace_real(u[mu])));
-  }
-
-  link = link / double(geom.Vol()*Nd*Nc);
+  link /= double(geom.Vol()*Nd*Nc);
 }
