34fc0253d39e680feba3d9cdfbdcfe6dd3244f53
edwards <edwards>
2003-01-17 05:42:06 +0000

    Added ArrayMapFunc.

--- include/map.h
+++ include/map.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: map.h,v 1.1 2002-12-14 01:07:47 edwards Exp $
+// $Id: map.h,v 1.2 2003-01-17 05:42:06 edwards Exp $
 
 /*! @file
  * @brief Map classes
@@ -27,6 +27,19 @@ public:
   //! Maps a lattice coordinate under a map to a new lattice coordinate
   /*! sign > 0 for map, sign < 0 for the inverse map */
   virtual multi1d<int> operator() (const multi1d<int>& coordinate, int sign) const = 0;
+};
+    
+//! ArrayMapFunc 
+/*! Abstract base class used as a function object for constructing maps */
+class ArrayMapFunc
+{
+public:
+  //! Maps a lattice coordinate under a map to a new lattice coordinate
+  /*! sign > 0 for map, sign < 0 for the inverse map */
+  virtual multi1d<int> operator() (const multi1d<int>& coordinate, int sign, int dir) const = 0;
+
+  //! Returns the array size - the number of directions which are to be used
+  virtual int numArray() const;
 };
     
 /** @} */ // end of group map
