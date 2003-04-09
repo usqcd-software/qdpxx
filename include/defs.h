f035a742cd92cac69d0cf73322ca08c3fc0d96dc
edwards <edwards>
2003-04-09 16:58:42 +0000

    Added some preproc. controls for precision.

--- include/defs.h
+++ include/defs.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: defs.h,v 1.7 2003-03-20 22:19:04 dgr Exp $
+// $Id: defs.h,v 1.8 2003-04-09 16:58:42 edwards Exp $
 
 /*! \file
  * \brief Type definitions
@@ -16,16 +16,42 @@ QDP_BEGIN_NAMESPACE(QDP);
  * @{
  */
 
+#if defined(SINGLE_PRECISION)
+#undef SINGLE_PRECISION
+#endif
+
+#if defined(DOUBLE_PRECISION)
+#undef DOUBLE_PRECISION
+#endif
+
+// Fix default precision
+#if ! defined(BASE_PRECISION)
+#define BASE_PRECISION SINGLE_PRECISION
+#endif
+
 // These are fixed precision versions
 #define INTEGER32 int
 #define REAL32    float
 #define REAL64    double
 #define LOGICAL   bool
 
-// These are floating precision versions
+// Set the base floating precision
+#if BASE_PRECISION == SINGLE_PRECISION
+// Use single precision for base precision
 #define REAL      REAL32
 #define DOUBLE    REAL64
 
+#elif BASE_PRECISION == DOUBLE_PRECISION
+// Use double precision for base precision
+#define REAL      REAL64
+#define DOUBLE    REAL64
+
+#else
+#error "Unknown BASE_PRECISION"
+#endif
+
+
+//----------------------------------------------------------------------
 //! Gamma matrices are conveniently defined for this Ns
 typedef GammaType<Ns> Gamma;
 
@@ -34,6 +60,7 @@ typedef OLattice< PScalar< PColorVector< RComplex<REAL>, Nc> > > LatticeColorVec
 typedef OLattice< PSpinVector< PScalar< RComplex<REAL> >, Ns> > LatticeSpinVector;
 typedef OLattice< PScalar< PColorMatrix< RComplex<REAL>, Nc> > > LatticeColorMatrix;
 typedef OLattice< PSpinMatrix< PScalar< RComplex<REAL> >, Ns> > LatticeSpinMatrix;
+typedef OLattice< PSpinVector< PColorVector< RComplex<REAL>, Nc>, 1> > LatticeStaggeredFermion;
 typedef OLattice< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns> > LatticeFermion;
 typedef OLattice< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns>>1 > > LatticeHalfFermion;
 typedef OLattice< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, Ns> > LatticePropagator;
@@ -49,6 +76,7 @@ typedef OScalar< PScalar< PColorVector< RComplex<REAL>, Nc> > > ColorVector;
 typedef OScalar< PScalar< PColorMatrix< RComplex<REAL>, Nc> > > ColorMatrix;
 typedef OScalar< PSpinVector< PScalar< RComplex<REAL> >, Ns> > SpinVector;
 typedef OScalar< PSpinMatrix< PScalar< RComplex<REAL> >, Ns> > SpinMatrix;
+typedef OScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, 1> > StaggeredFermion;
 typedef OScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns> > Fermion;
 typedef OScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns>>1 > > HalfFermion;
 typedef OScalar< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, Ns> > Propagator;
@@ -79,6 +107,16 @@ typedef OScalar< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > ColorMatrixF;
 
 
 typedef OScalar< PScalar< PScalar< RScalar<REAL32> > > > Real32;
+typedef OScalar< PScalar< PScalar< RScalar<REAL64> > > > Real64;
+
+// Equivalent names
+typedef Integer  Int;
+
+typedef Real32  RealF;
+typedef Real64  RealD;
+
+typedef LatticeInteger  LatticeInt;
+
 
 /*! @} */   // end of group defs
 
