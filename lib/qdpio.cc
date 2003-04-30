64f2dc6106502636f331a5de9440212296e4f7d1
edwards <edwards>
2003-04-30 21:09:48 +0000

    Changed char* usages to string&.

--- lib/qdpio.cc
+++ lib/qdpio.cc
@@ -1,4 +1,4 @@
-// $Id: qdpio.cc,v 1.5 2003-04-17 18:39:54 edwards Exp $
+// $Id: qdpio.cc,v 1.6 2003-04-30 21:09:48 edwards Exp $
 //
 /*! @file
  * @brief IO support via QIO
@@ -50,9 +50,9 @@ static int get_node_number(const int coord[])
 // QDP QIO support
 QDPSerialReader::QDPSerialReader() {iop=false;}
 
-QDPSerialReader::QDPSerialReader(XMLMetaReader& xml, const char* p) {open(xml,p);}
+QDPSerialReader::QDPSerialReader(XMLMetaReader& xml, const std::string& p) {open(xml,p);}
 
-void QDPSerialReader::open(XMLMetaReader& xml, const char* p) 
+void QDPSerialReader::open(XMLMetaReader& xml, const std::string& p) 
 {
   QIO_Layout *layout = new QIO_Layout;
   int latsize[Nd];
@@ -66,7 +66,7 @@ void QDPSerialReader::open(XMLMetaReader& xml, const char* p)
   layout->volume = Layout::vol(); 
   layout->this_node = Layout::nodeNumber(); 
 
-  if ((qio_in = QIO_open_read(xml.get(), p, QIO_SERIAL, layout)) == NULL)
+  if ((qio_in = QIO_open_read(xml.get(), p.c_str(), QIO_SERIAL, layout)) == NULL)
     QDP_error_exit("QDPSerial::Reader: failed to open file %s",p);
 
   delete layout;
@@ -97,9 +97,9 @@ QDPSerialReader::~QDPSerialReader() {close();}
 //! text writer support
 QDPSerialWriter::QDPSerialWriter() {iop=false;}
 
-QDPSerialWriter::QDPSerialWriter(const XMLMetaWriter& xml, const char* p) {open(xml,p);}
+QDPSerialWriter::QDPSerialWriter(const XMLMetaWriter& xml, const std::string& p) {open(xml,p);}
 
-void QDPSerialWriter::open(const XMLMetaWriter& xml, const char* p) 
+void QDPSerialWriter::open(const XMLMetaWriter& xml, const std::string& p) 
 {
   QIO_Layout *layout = new QIO_Layout;
   int latsize[Nd];
@@ -113,7 +113,7 @@ void QDPSerialWriter::open(const XMLMetaWriter& xml, const char* p)
   layout->volume = Layout::vol(); 
   layout->this_node = Layout::nodeNumber(); 
 
-  if ((qio_out = QIO_open_write(xml.get(), p, QIO_SERIAL, QIO_LEX_ORDER, QIO_CREATE, 
+  if ((qio_out = QIO_open_write(xml.get(), p.c_str(), QIO_SERIAL, QIO_LEX_ORDER, QIO_CREATE, 
 				layout)) == NULL)
     QDP_error_exit("QDPSerial::Writer: failed to open file %s",p);
 
