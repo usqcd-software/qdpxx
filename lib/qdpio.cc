936bb7c17425956cf645c8cedd4cf0990430b3d6
edwards <edwards>
2003-05-10 23:17:04 +0000

    Added true xml IO support. Needs some more work.

--- lib/qdpio.cc
+++ lib/qdpio.cc
@@ -1,4 +1,4 @@
-// $Id: qdpio.cc,v 1.7 2003-05-08 03:32:57 edwards Exp $
+// $Id: qdpio.cc,v 1.8 2003-05-10 23:17:04 edwards Exp $
 //
 /*! @file
  * @brief IO support via QIO
@@ -7,34 +7,6 @@
 #include "qdp.h"
 
 QDP_BEGIN_NAMESPACE(QDP);
-
-//--------------------------------------------------------------------------------
-// XML classes
-//! XML reader class
-XMLMetaReader::XMLMetaReader()
-{
-  // HACK - set string to something trivial
-  xml = XML_create(MAX_XML);
-}
-
-XMLMetaReader::~XMLMetaReader()
-{
-  XML_destroy(xml);
-}
-
-//! XML writer class
-XMLMetaWriter::XMLMetaWriter()
-{
-  // HACK - set string to something trivial
-  xml = XML_create(MAX_XML);
-}
-
-XMLMetaWriter::~XMLMetaWriter()
-{
-  XML_destroy(xml);
-}
-
-
 
 //-----------------------------------------
 static int get_node_number(const int coord[])
@@ -50,9 +22,9 @@ static int get_node_number(const int coord[])
 // QDP QIO support
 QDPSerialReader::QDPSerialReader() {iop=false;}
 
-QDPSerialReader::QDPSerialReader(XMLMetaReader& xml, const std::string& p) {open(xml,p);}
+QDPSerialReader::QDPSerialReader(XMLReader& xml, const std::string& p) {open(xml,p);}
 
-void QDPSerialReader::open(XMLMetaReader& xml, const std::string& p) 
+void QDPSerialReader::open(XMLReader& xml, const std::string& p) 
 {
   QIO_Layout *layout = new QIO_Layout;
   int latsize[Nd];
@@ -66,7 +38,15 @@ void QDPSerialReader::open(XMLMetaReader& xml, const std::string& p)
   layout->volume = Layout::vol(); 
   layout->this_node = Layout::nodeNumber(); 
 
-  if ((qio_in = QIO_open_read(xml.get(), p.c_str(), QIO_SERIAL, layout)) == NULL)
+  // Grab metadata
+  ostringstream  xmlstr;
+  xml.print(xmlstr);
+
+  // Copy metadata string into simple qio string container
+  XML_MetaData* xml_c = XML_create(xmlstr.str().length()+1);  // check if +1 is needed
+  XML_set(xml_c, xmlstr.str().c_str());
+
+  if ((qio_in = QIO_open_read(xml_c, p.c_str(), QIO_SERIAL, layout)) == NULL)
     QDP_error_exit("QDPSerial::Reader: failed to open file %s",p.c_str());
 
   delete layout;
@@ -116,10 +96,17 @@ void QDPSerialWriter::open(const XMLMetaWriter& xml, const std::string& p)
   layout->volume = Layout::vol(); 
   layout->this_node = Layout::nodeNumber(); 
 
-  if ((qio_out = QIO_open_write(xml.get(), p.c_str(), QIO_SERIAL, QIO_LEX_ORDER, QIO_CREATE, 
+  // Copy metadata string into simple qio string container
+  XML_MetaData* xml_c = XML_create(xml.str().length()+1);  // check if +1 is needed
+  XML_set(xml_c, xml.str().c_str());
+
+  // Big call to qio
+  if ((qio_out = QIO_open_write(xml_c, p.c_str(), QIO_SERIAL, QIO_LEX_ORDER, QIO_CREATE, 
 				layout)) == NULL)
     QDP_error_exit("QDPSerial::Writer: failed to open file %s",p.c_str());
 
+  // Cleanup
+  XML_destroy(xml_c);
   delete layout;
 
   iop=true;
