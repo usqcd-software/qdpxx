2f8edf2c02aa33c90aedf7ad6e8e680c664dc1a2
edwards <edwards>
2003-08-23 03:05:50 +0000

    Added another sranf function for inner grid support.

--- lib/qdp_random.cc
+++ lib/qdp_random.cc
@@ -1,4 +1,4 @@
-// $Id: qdp_random.cc,v 1.3 2003-07-31 01:00:30 edwards Exp $
+// $Id: qdp_random.cc,v 1.4 2003-08-23 03:05:50 edwards Exp $
 //
 // Random number generator support
 
@@ -203,6 +203,55 @@ namespace RNG
     skewed_seed = ran_tmp;
 
     return _ssranf;
+  }
+
+
+  //! Scalar random number generator. Done on the front end. */
+  /*! 
+   * It is linear congruential with modulus m = 2**47, increment c = 0,
+   * and multiplier a = (2**36)*m3 + (2**24)*m2 + (2**12)*m1 + m0.  
+   */
+  void sranf(float* d, int N, Seed& seed, ILatticeSeed& skewed_seed, const Seed& seed_mult)
+  {
+    /* Calculate the random number and update the seed according to the
+     * following algorithm
+     *
+     * FILL(twom11,TWOM11);
+     * FILL(twom12,TWOM12);
+     * i3 = ran_seed(3)*ran_mult(0) + ran_seed(2)*ran_mult(1)
+     *    + ran_seed(1)*ran_mult(2) + ran_seed(0)*ran_mult(3);
+     * i2 = ran_seed(2)*ran_mult(0) + ran_seed(1)*ran_mult(1)
+     *    + ran_seed(0)*ran_mult(2);
+     * i1 = ran_seed(1)*ran_mult(0) + ran_seed(0)*ran_mult(1);
+     * i0 = ran_seed(0)*ran_mult(0);
+     *
+     * ran_seed(0) = mod(i0, 4096);
+     * i1          = i1 + i0/4096;
+     * ran_seed(1) = mod(i1, 4096);
+     * i2          = i2 + i1/4096;
+     * ran_seed(2) = mod(i2, 4096);
+     * ran_seed(3) = mod(i3 + i2/4096, 2048);
+     *
+     * sranf = twom11*(TO_REAL32(VALUE(ran_seed(3)))
+     *       + twom12*(TO_REAL32(VALUE(ran_seed(2)))
+     *       + twom12*(TO_REAL32(VALUE(ran_seed(1)))
+     *       + twom12*(TO_REAL32(VALUE(ran_seed(0)))))));
+     */
+    ILatticeReal _sranf;
+    Seed ran_tmp1;
+    ILatticeSeed ran_tmp2;
+
+    _sranf = seedToFloat(skewed_seed);
+    for(int i=0; i < N; ++i)
+    {
+      cast_rep(d[i], getSite(_sranf,i));
+    }
+
+    ran_tmp1 = seed * seed_mult;
+    seed = ran_tmp1;
+
+    ran_tmp2 = skewed_seed * seed_mult;
+    skewed_seed = ran_tmp2;
   }
 
 };
