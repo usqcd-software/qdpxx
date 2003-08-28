42ed990db277d2d19f132766c0f39de0948c9b46
edwards <edwards>
2003-08-28 18:02:26 +0000

    Removed consts in front of XMLBufferWriter.

--- include/qdp_qdpio.h
+++ include/qdp_qdpio.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: qdp_qdpio.h,v 1.2 2003-05-23 04:42:03 edwards Exp $
+// $Id: qdp_qdpio.h,v 1.3 2003-08-28 18:02:26 edwards Exp $
 
 /*! @file
  * @brief IO support via QIO
@@ -101,10 +101,10 @@ public:
   ~QDPSerialFileWriter();
 
   //! Open file
-  explicit QDPSerialFileWriter(const XMLBufferWriter& xml, const std::string& p);
+  explicit QDPSerialFileWriter(XMLBufferWriter& xml, const std::string& p);
 
   //! Open file
-  void open(const XMLBufferWriter& xml, const std::string& p);
+  void open(XMLBufferWriter& xml, const std::string& p);
 
   //! Close file
   void close();
@@ -114,20 +114,20 @@ public:
 
   //! Write a QDP object
   template<class T, class C>
-  void write(const XMLBufferWriter& xml, const QDPType<T,C>& s1)
+  void write(XMLBufferWriter& xml, const QDPType<T,C>& s1)
     {write(*this,xml,static_cast<const C&>(s1));}
 
   //! Write an OScalar object
   template<class T>
-  void write(const XMLBufferWriter& xml, const OScalar<T>& s1);
+  void write(XMLBufferWriter& xml, const OScalar<T>& s1);
 
   //! Write an OLattice object
   template<class T>
-  void write(const XMLBufferWriter& xml, const OLattice<T>& s1);
+  void write(XMLBufferWriter& xml, const OLattice<T>& s1);
 
   //! Write an array of objects each in a separate record
   template<class T>
-  void write(const XMLBufferWriter& xml, const multi1d<T>& s1)
+  void write(XMLBufferWriter& xml, const multi1d<T>& s1)
     {
       for(int i=0; i < s1.size(); ++i)
 	write(xml,s1[i]);
@@ -135,7 +135,7 @@ public:
 
   //! Write an array of objects all in a single record
   template<class T>
-  void vwrite(const XMLBufferWriter& xml, const multi1d<T>& s1) {}
+  void vwrite(XMLBufferWriter& xml, const multi1d<T>& s1) {}
 
   //!  Check if an unrecoverable error has occurred
   bool bad() const;
@@ -152,7 +152,7 @@ private:
 
 //! Write an OLattice object
 template<class T>
-void write(QDPSerialFileWriter& qsw, const XMLBufferWriter& rec_xml, const OLattice<T>& s1)
+void write(QDPSerialFileWriter& qsw, XMLBufferWriter& rec_xml, const OLattice<T>& s1)
 {
   qsw.write(rec_xml,s1);
 }
@@ -230,22 +230,32 @@ template<class T> void QDPFactoryGet(char *buf, const int crd[], void *arg)
 //! Write an OLattice object
 /*! This implementation is only correct for scalar ILattice */
 template<class T>
-void QDPSerialFileWriter::write(const XMLBufferWriter& rec_xml, const OLattice<T>& s1)
+void QDPSerialFileWriter::write(XMLBufferWriter& rec_xml, const OLattice<T>& s1)
 {
+//  cout << "QDPSerialFileWriter::write" << endl;
+
   // Copy metadata string into simple qio string container
-  XMLBufferWriter& foo_xml = const_cast<XMLBufferWriter&>(rec_xml);
-  XML_string* xml_c  = XML_string_create(foo_xml.str().length()+1);  // check if +1 is needed
-  XML_string_set(xml_c, foo_xml.str().c_str());
+  XML_string* xml_c  = XML_string_create(rec_xml.str().length()+1);  // check if +1 is needed
+  XML_string_set(xml_c, rec_xml.str().c_str());
+
+//  cout << "len=" << (rec_xml.str().length()+1) << endl;
+//  cout << "rec_c= XXX" << xml_c << "XXX" << endl;
 
   // For now, create an empty binX field
   string binx = "Empty binX entry";
   XML_string* BinX_c = XML_string_create(binx.length()+1);  // check if +1 is needed
   XML_string_set(BinX_c, binx.c_str());
 
+//  cout << "len=" << (rec_xml.str().length()+1) << endl;
+//  cout << "BinX_c= XXX" << xml_c << "XXX" << endl;
+
   // Big call to qio
   int status = QIO_write(get(), xml_c, BinX_c, 
 			 &(QDPFactoryGet<OLattice<T> >),
                          sizeof(T), (void *)&s1);
+
+//  cout << "cleanup in serialfile" << endl;
+
   // Cleanup
   XML_string_destroy(BinX_c);
   XML_string_destroy(xml_c);
