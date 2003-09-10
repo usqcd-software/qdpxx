379d44ba6a05813cca7bf390cb518e92adfd27a9
edwards <edwards>
2003-09-10 01:45:15 +0000

    Not finished test code for various QDP exotica.

---
+++ examples/t_exotic.cc
@@ -0,0 +1,63 @@
+// $Id: t_exotic.cc,v 1.1 2003-09-10 01:45:15 edwards Exp $
+
+#include <iostream>
+#include <cstdio>
+
+#include "qdp.h"
+#include "examples.h"
+
+using namespace QDP;
+
+int main(int argc, char *argv[])
+{
+  // Put the machine into a known state
+  QDP_initialize(&argc, &argv);
+
+  // Setup the layout
+  const int foo[] = {4,4,4,4};
+  multi1d<int> nrow(Nd);
+  nrow = foo;  // Use only Nd elements
+  Layout::setLattSize(nrow);
+  Layout::create();
+
+  NmlWriter nml("t_mesplq.nml");
+
+  push(nml,"lattis");
+  Write(nml,Nd);
+  Write(nml,Nc);
+  Write(nml,nrow);
+  pop(nml);
+
+  //! Example of calling a plaquette routine
+  /*! NOTE: the STL is *not* used to hold gauge fields */
+  multi1d<LatticeColorMatrix> u(Nd);
+  Double w_plaq, s_plaq, t_plaq, link;
+
+  cout << "Start gaussian\n";
+  for(int m=0; m < u.size(); ++m)
+    gaussian(u[m]);
+
+  // Reunitarize the gauge field
+  cout << "Start reunit\n";
+  for(int m=0; m < u.size(); ++m)
+    reunit(u[m]);
+
+  // Try out the plaquette routine
+  cout << "Start mesplq\n";
+  MesPlq(u, w_plaq, s_plaq, t_plaq, link);
+  cout << "w_plaq = " << w_plaq << endl;
+  cout << "link = " << link << endl;
+
+  // Write out the results
+  push(nml,"observables");
+  Write(nml,w_plaq);
+  Write(nml,link);
+  pop(nml);
+
+  nml.flush();
+
+  // Time to bolt
+  QDP_finalize();
+
+  exit(0);
+}
