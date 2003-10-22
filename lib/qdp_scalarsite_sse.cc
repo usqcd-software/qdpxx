7246429429a2a92e011c9e35de26fb2ac96e2fdd
edwards <edwards>
2003-10-22 02:44:46 +0000

    Changed to have new inner index.

--- lib/qdp_scalarsite_sse.cc
+++ lib/qdp_scalarsite_sse.cc
@@ -1,4 +1,4 @@
-// $Id: qdp_scalarsite_sse.cc,v 1.4 2003-10-22 00:57:54 edwards Exp $
+// $Id: qdp_scalarsite_sse.cc,v 1.5 2003-10-22 02:44:46 edwards Exp $
 
 /*! @file
  * @brief Intel SSE optimizations
@@ -17,26 +17,26 @@ QDP_BEGIN_NAMESPACE(QDP);
 // Specialization to optimize the case   
 //    LatticeColorMatrix[OrderedSubset] = LatticeColorMatrix * LatticeColorMatrix
 template<>
-void evaluate(OLattice<PScalar<PColorMatrix<RComplexFloat, 3> > >& d, 
+void evaluate(OLattice<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > > >& d, 
 	      const OpAssign& op, 
 	      const QDPExpr<BinaryNode<OpMultiply, 
-	      Reference<QDPType<PScalar<PColorMatrix<RComplexFloat, 3> >, 
-	      OLattice<PScalar<PColorMatrix<RComplexFloat, 3> > > > >, 
-	      Reference<QDPType<PScalar<PColorMatrix<RComplexFloat, 3> >, 
-	      OLattice<PScalar<PColorMatrix<RComplexFloat, 3> > > > > >,
-	      OLattice<PScalar<PColorMatrix<RComplexFloat, 3> > > >& rhs,
+	      Reference<QDPType<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > >, 
+	      OLattice<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > > > > >, 
+	      Reference<QDPType<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > >, 
+	      OLattice<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > > > > > >,
+	      OLattice<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > > > >& rhs,
 	      const OrderedSubset& s)
 {
-// cout << "call single site QDP_M_eq_M_times_M" << endl;
+//  cout << "call single site QDP_M_eq_M_times_M" << endl;
 
-  typedef OLattice<PScalar<PColorMatrix<RComplexFloat, 3> > >       C;
+  typedef OLattice<PScalar<PScalar<PColorMatrix<RComplexFloat, 3> > > >    C;
 
   const C& l = static_cast<const C&>(rhs.expression().left());
   const C& r = static_cast<const C&>(rhs.expression().right());
 
   for(int i=s.start(); i <= s.end(); ++i) 
   {
-    _inline_sse_mult_su3_nn(l.elem(i).elem(),r.elem(i).elem(),d.elem(i).elem());
+    _inline_sse_mult_su3_nn(l.elem(i).elem().elem(),r.elem(i).elem().elem(),d.elem(i).elem().elem());
   }
 }
 
