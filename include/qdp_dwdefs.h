3a3a8f9d653b30ecbf8c3fb1d9523e8d048f2d75
edwards <edwards>
2003-11-02 02:01:09 +0000

    Moved PDWVector index to the innermost index.

--- include/qdp_dwdefs.h
+++ include/qdp_dwdefs.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: qdp_dwdefs.h,v 1.3 2003-10-22 00:59:44 edwards Exp $
+// $Id: qdp_dwdefs.h,v 1.4 2003-11-02 02:01:09 edwards Exp $
 
 /*! \file
  * \brief Type definitions
@@ -53,68 +53,68 @@ typedef GammaType<Ns> Gamma;
 // Aliases for a scalar architecture
 
 // Fixed fermion type
-typedef OLattice< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, 4> > > LatticeDiracFermion;
-typedef OLattice< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, 1> > > LatticeStaggeredFermion;
-typedef OLattice< PScalar< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, 4> > > LatticeDiracPropagator;
+typedef OLattice< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, 4> > LatticeDiracFermion;
+typedef OLattice< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, 1> > LatticeStaggeredFermion;
+typedef OLattice< PSpinMatrix< PColorMatrix< RComplex< PScalar<REAL> >, Nc>, 4> > LatticeDiracPropagator;
 
 // Floating aliases
-typedef OLattice< PScalar< PScalar< PColorVector< RComplex<REAL>, Nc> > > > LatticeColorVector;
-typedef OLattice< PScalar< PSpinVector< PScalar< RComplex<REAL> >, Ns> > > LatticeSpinVector;
-typedef OLattice< PScalar< PScalar< PColorMatrix< RComplex<REAL>, Nc> > > > LatticeColorMatrix;
-typedef OLattice< PScalar< PSpinMatrix< PScalar< RComplex<REAL> >, Ns> > > LatticeSpinMatrix;
-typedef OLattice< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns> > > LatticeFermion;
-typedef OLattice< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns>>1 > > > LatticeHalfFermion;
-typedef OLattice< PScalar< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, Ns> > > LatticePropagator;
-typedef OLattice< PScalar< PScalar< PScalar< RComplex<REAL> > > > > LatticeComplex;
+typedef OLattice< PScalar< PColorVector< RComplex< PScalar<REAL> >, Nc> > > LatticeColorVector;
+typedef OLattice< PSpinVector< PScalar< RComplex< PScalar<REAL> > >, Ns> > LatticeSpinVector;
+typedef OLattice< PScalar< PColorMatrix< RComplex< PScalar<REAL> >, Nc> > > LatticeColorMatrix;
+typedef OLattice< PSpinMatrix< PScalar< RComplex< PScalar<REAL> > >, Ns> > LatticeSpinMatrix;
+typedef OLattice< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, Ns> > LatticeFermion;
+typedef OLattice< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, Ns>>1 > > LatticeHalfFermion;
+typedef OLattice< PSpinMatrix< PColorMatrix< RComplex< PScalar<REAL> >, Nc>, Ns> > LatticePropagator;
+typedef OLattice< PScalar< PScalar< RComplex< PScalar<REAL> > > > > LatticeComplex;
 
-typedef OLattice< PScalar< PScalar< PSeed < RScalar<INTEGER32> > > > > LatticeSeed;
-typedef OLattice< PScalar< PScalar< PScalar< RScalar<INTEGER32> > > > > LatticeInteger;
-typedef OLattice< PScalar< PScalar< PScalar< RScalar<REAL> > > > > LatticeReal;
-typedef OLattice< PScalar< PScalar< PScalar< RScalar<DOUBLE> > > > > LatticeDouble;
-typedef OLattice< PScalar< PScalar< PScalar< RScalar<LOGICAL> > > > > LatticeBoolean;
+typedef OLattice< PScalar< PSeed < RScalar< PScalar<INTEGER32> > > > > LatticeSeed;
+typedef OLattice< PScalar< PScalar< RScalar< PScalar<INTEGER32> > > > > LatticeInteger;
+typedef OLattice< PScalar< PScalar< RScalar< PScalar<REAL> > > > > LatticeReal;
+typedef OLattice< PScalar< PScalar< RScalar< PScalar<DOUBLE> > > > > LatticeDouble;
+typedef OLattice< PScalar< PScalar< RScalar< PScalar<LOGICAL> > > > > LatticeBoolean;
 
-typedef OScalar< PScalar< PScalar< PColorVector< RComplex<REAL>, Nc> > > > ColorVector;
-typedef OScalar< PScalar< PScalar< PColorMatrix< RComplex<REAL>, Nc> > > > ColorMatrix;
-typedef OScalar< PScalar< PSpinVector< PScalar< RComplex<REAL> >, Ns> > > SpinVector;
-typedef OScalar< PScalar< PSpinMatrix< PScalar< RComplex<REAL> >, Ns> > > SpinMatrix;
-typedef OScalar< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, 1> > > StaggeredFermion;
-typedef OScalar< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns> > > Fermion;
-typedef OScalar< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns>>1 > > > HalfFermion;
-typedef OScalar< PScalar< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, Ns> > > Propagator;
-typedef OScalar< PScalar< PScalar< PScalar< RComplex<REAL> > > > > Complex;
+typedef OScalar< PScalar< PColorVector< RComplex< PScalar<REAL> >, Nc> > > ColorVector;
+typedef OScalar< PScalar< PColorMatrix< RComplex< PScalar<REAL> >, Nc> > > ColorMatrix;
+typedef OScalar< PSpinVector< PScalar< RComplex< PScalar<REAL> > >, Ns> > SpinVector;
+typedef OScalar< PSpinMatrix< PScalar< RComplex< PScalar<REAL> > >, Ns> > SpinMatrix;
+typedef OScalar< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, 1> > StaggeredFermion;
+typedef OScalar< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, Ns> > Fermion;
+typedef OScalar< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, Ns>>1 > > HalfFermion;
+typedef OScalar< PSpinMatrix< PColorMatrix< RComplex< PScalar<REAL> >, Nc>, Ns> > Propagator;
+typedef OScalar< PScalar< PScalar< RComplex< PScalar<REAL> > > > > Complex;
 
-typedef OScalar< PScalar< PScalar< PSeed< RScalar<INTEGER32> > > > > Seed;
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<INTEGER32> > > > > Integer;
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<REAL> > > > > Real;
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<DOUBLE> > > > > Double;
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<LOGICAL> > > > > Boolean;
+typedef OScalar< PScalar< PSeed< RScalar< PScalar<INTEGER32> > > > > Seed;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<INTEGER32> > > > > Integer;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<REAL> > > > > Real;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<DOUBLE> > > > > Double;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<LOGICAL> > > > > Boolean;
 
-typedef OScalar< PScalar< PScalar< PScalar< RComplex<DOUBLE> > > > > DComplex;
+typedef OScalar< PScalar< PScalar< RComplex< PScalar<DOUBLE> > > > > DComplex;
 
 
 // Other useful names
-typedef OScalar< PScalar< PSpinVector< PColorVector< RComplex<REAL>, Nc>, Ns> > > ColorVectorSpinVector;
-typedef OScalar< PScalar< PSpinMatrix< PColorMatrix< RComplex<REAL>, Nc>, Ns> > > ColorMatrixSpinMatrix;
+typedef OScalar< PSpinVector< PColorVector< RComplex< PScalar<REAL> >, Nc>, Ns> > ColorVectorSpinVector;
+typedef OScalar< PSpinMatrix< PColorMatrix< RComplex< PScalar<REAL> >, Nc>, Ns> > ColorMatrixSpinMatrix;
 
 // Level below outer for internal convenience
-typedef PScalar< PScalar< PScalar< RScalar<REAL> > > > IntReal;
-typedef PScalar< PScalar< PScalar< RScalar<INTEGER32> > > > IntInteger;
-typedef PScalar< PScalar< PScalar< RScalar<DOUBLE> > > > IntDouble;
-typedef PScalar< PScalar< PScalar< RScalar<LOGICAL> > > > IntBoolean;
+typedef PScalar< PScalar< RScalar< PScalar<REAL> > > > IntReal;
+typedef PScalar< PScalar< RScalar< PScalar<INTEGER32> > > > IntInteger;
+typedef PScalar< PScalar< RScalar< PScalar<DOUBLE> > > > IntDouble;
+typedef PScalar< PScalar< RScalar< PScalar<LOGICAL> > > > IntBoolean;
 
 // Odd-ball to support random numbers
 typedef Real ILatticeReal;
 typedef Seed ILatticeSeed;
 
 // Fixed precision
-typedef OLattice< PScalar< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > > LatticeColorMatrixF;
-typedef OScalar< PScalar< PScalar< PColorMatrix< RComplex<REAL32>, Nc> > > > ColorMatrixF;
+typedef OLattice< PScalar< PColorMatrix< RComplex< PScalar<REAL32> >, Nc> > > LatticeColorMatrixF;
+typedef OScalar< PScalar< PColorMatrix< RComplex< PScalar<REAL32> >, Nc> > > ColorMatrixF;
 
 
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<REAL32> > > > > Real32;
-typedef OScalar< PScalar< PScalar< PScalar< RScalar<REAL64> > > > > Real64;
-typedef OScalar< PScalar< PScalar< PScalar< RComplex<REAL32> > > > > Complex32;
-typedef OScalar< PScalar< PScalar< PScalar< RComplex<REAL64> > > > > Complex64;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<REAL32> > > > > Real32;
+typedef OScalar< PScalar< PScalar< RScalar< PScalar<REAL64> > > > > Real64;
+typedef OScalar< PScalar< PScalar< RComplex< PScalar<REAL32> > > > > Complex32;
+typedef OScalar< PScalar< PScalar< RComplex< PScalar<REAL64> > > > > Complex64;
 
 // Equivalent names
 typedef Integer  Int;
