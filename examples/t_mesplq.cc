b8715a9cb5f0cb869b02f2b016ddfa0f2add6eb0
edwards <edwards>
2004-02-19 02:25:04 +0000

    Pulled out the previous nml version and re-inserted it here. Not ready for the big change to XML (coward I guess...).

--- examples/t_mesplq.cc
+++ examples/t_mesplq.cc
@@ -1,4 +1,4 @@
-// $Id: t_mesplq.cc,v 1.18 2004-02-03 15:11:33 bjoo Exp $
+// $Id: t_mesplq.cc,v 1.19 2004-02-19 02:25:04 edwards Exp $
 
 #include <iostream>
 #include <cstdio>
@@ -20,21 +20,14 @@ int main(int argc, char *argv[])
   Layout::setLattSize(nrow);
   Layout::create();
 
-
-  XMLBufferWriter nml;
-  push(nml, "mesplqTest");
+  NmlWriter nml("t_mesplq.nml");
 
   push(nml,"lattis");
-  try { 
-  write(nml, "Nd" , Nd);
-  write(nml, "Nc", Nc);
-  write(nml, "nrow", nrow);
+  Write(nml,Nd);
+  Write(nml,Nc);
+  Write(nml,nrow);
   pop(nml);
-  }
-  catch ( const std::string& e ) { 
-	QDPIO::cout << "exception raised : " << e << endl;
-  }
-    
+
   //! Example of calling a plaquette routine
   /*! NOTE: the STL is *not* used to hold gauge fields */
   multi1d<LatticeColorMatrix> u(Nd);
@@ -55,28 +48,14 @@ int main(int argc, char *argv[])
   QDPIO::cout << "w_plaq = " << w_plaq << endl;
   QDPIO::cout << "link = " << link << endl;
 
-
   // Write out the results
-  try { 
   push(nml,"observables");
-  write(nml, "w_plaq", w_plaq);
-  write(nml, "link", link);
+  Write(nml,w_plaq);
+  Write(nml,link);
   pop(nml);
- 
-  pop(nml); // Pop root tag
-  } 
-  catch (const std::string& e) { 
-    QDPIO::cout << "Exception Raised : " << e << endl;
-  }
- 
-  QDPIO::cout << "XML Output" << endl ;
-  std::string str;
-  try { 
-    QDPIO::cout <<  nml.printRoot() << endl;
-  }
-  catch (const std::string& e) {
-     QDPIO::cerr << "Exception raised : " << e << endl;
-  }
+
+  nml.flush();
+
   // Time to bolt
   QDP_finalize();
 
