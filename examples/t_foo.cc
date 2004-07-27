ffd34fa7ce58946bf64c8555d7fb0532741acf4c
edwards <edwards>
2004-07-27 05:38:10 +0000

    Tests of profiling.

--- examples/t_foo.cc
+++ examples/t_foo.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 //
-// $Id: t_foo.cc,v 1.39 2004-07-12 16:33:28 edwards Exp $
+// $Id: t_foo.cc,v 1.40 2004-07-27 05:38:10 edwards Exp $
 //
 /*! \file
  *  \brief Silly little internal test code
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
   QDP_initialize(&argc, &argv);
 
   // Setup the layout
-  const int foo[] = {2,1,1,1};
+  const int foo[] = {8,8,4,4};
   multi1d<int> nrow(Nd);
   nrow = foo;  // Use only Nd elements
   Layout::setLattSize(nrow);
@@ -35,32 +35,70 @@ int main(int argc, char *argv[])
   write(xml,"nrow", nrow);
   write(xml,"logicalSize",Layout::logicalSize());
 
-#if 0
+  QDP_PUSH_PROFILE(QDP::getProfileLevel());
+
+#if 1
+  {
+    Real a, b, c;
+    random(a); random(b); random(c);
+
+    QDPIO::cout << "Simple scalars" << endl;
+    b = a;
+    c = a*b;
+    QDPIO::cout << "Try again" << endl;
+//    printExprTree(cout, c, OpAssign(), a*b); QDPIO::cout << endl;
+    
+    QDPIO::cout << "Try global sums" << endl;
+    QDP_PUSH_PROFILE(QDP::getProfileLevel());
+    c = sum(a);
+    c = sum(a*a);
+    c = norm2(a*a);
+    QDP_POP_PROFILE();
+    QDPIO::cout << "Done with simple scalars" << endl << endl << endl;
+  }
+#endif
+
+  QDP_POP_PROFILE();
+
+  QDP_PUSH_PROFILE(QDP::getProfileLevel());
+
+#if 1
   {
     LatticeColorMatrix a,b,c;
     LatticeComplex cc, dd;
+    Real fred;
     random(a); random(b); random(c);
 
     c = a*b;
     cc = trace(c);
-    cerr << "Here 1" << endl;
+    QDPIO::cout << "Here 1" << endl;
     dd = trace(a*b);
-    cerr << "Here 2" << endl;
+    QDPIO::cout << "Here 2" << endl;
     dd = trace(a*(b*1));
-    cerr << "Here 3" << endl;
+    QDPIO::cout << "Here 3" << endl;
     write(xml,"diff", Real(norm2(cc-dd)));
 
     c = adj(a)*b;
     cc = trace(c);
-    cerr << "Here 4" << endl;
+    QDPIO::cout << "Here 4" << endl;
     dd = trace(adj(a*b)*b*a*b);
-    cerr << "Here 5" << endl;
+    QDPIO::cout << "Here 5" << endl;
     write(xml,"diff", Real(norm2(cc-dd)));
     dd = localInnerProduct(a,b);
-    cerr << "Here 6" << endl;
+    QDPIO::cout << "Here 6" << endl;
     write(xml,"diff", Real(norm2(cc-dd)));
+
+    QDPIO::cout << "Try global sums" << endl;
+    fred = sum(real(trace(a)));
+    fred = sum(real(trace(adj(a)*b)));
+    fred = norm2(a);
+    fred = norm2(a*b);
   }
 #endif
+
+  QDP_POP_PROFILE();
+
+  QDP_PUSH_PROFILE(QDP::getProfileLevel());
 
 #if 1
   {
@@ -83,37 +121,44 @@ int main(int argc, char *argv[])
 //    LatticeComplex ps_rho = trace(adj(q) * gamma_1);
 //    LatticeComplex ps_rho = localInnerProduct(q, gamma_1);
 
-    cerr << "Here 1" << endl;
+    QDPIO::cout << "Here 1" << endl;
     LatticeComplex b = trace(S * traceColor(q * di_quark));
 
-    cerr << "Here 2" << endl;
+    QDPIO::cout << "Here 2" << endl;
     di_quark = quarkContract13(q * Gamma(5), Gamma(5) * q);
     LatticeComplex c = trace(S * traceColor(q * traceSpin(di_quark)));
 
-    cerr << "Here 3" << endl;
+    QDPIO::cout << "Here 3" << endl;
     LatticeComplex d = trace(S * traceColor(q * traceSpin(quarkContract13(q * Gamma(5), 
 									  Gamma(5) * q))));
 
-    cerr << "Here 4" << endl;
+    QDPIO::cout << "Here 4" << endl;
     write(xml,"diff", Real(norm2(c-d)));
 
     c = trace(q * di_quark);
     LatticeReal r = real(c);
 
-    cerr << "Here 5" << endl;
+    QDPIO::cout << "Here 5" << endl;
     LatticeReal s = real(trace(q * di_quark));
     
-    cerr << "Here 6" << endl;
+    QDPIO::cout << "Here 6" << endl;
     write(xml,"diff", Real(norm2(r-s)));
 
-    cerr << "Here 7" << endl;
+    QDPIO::cout << "Here 7" << endl;
     s = real(trace(adj(q) * di_quark * q));
 
-    cerr << "Here 8" << endl;
+    QDPIO::cout << "Here 8" << endl;
     s = real(trace(q * di_quark * q));
+
+    QDPIO::cout << "Here 9" << endl;
+    LatticeFermion psi, chi;
+    random(psi); random(chi);
+    Real InvTwoKappa = 0.5;
+    chi = GammaConst<Ns,Ns*Ns-1>()*psi;
   }
 #endif
   
+  QDP_POP_PROFILE();
 
   pop(xml);
   xml.close();
