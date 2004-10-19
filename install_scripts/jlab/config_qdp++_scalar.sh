05cad54cedc5f766c53e4ef3b91cd06619841e35
edwards <edwards>
2004-10-19 01:03:10 +0000

    JLab install scripts

--- install_scripts/jlab/config_qdp++_scalar.sh
+++ install_scripts/jlab/config_qdp++_scalar.sh
@@ -1,3 +1,3 @@
-#!/bin/csh
+#!/bin/sh
 
 ../configure --prefix=/usr/local/qdp++/scalar --enable-parallel-arch=scalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" LDFLAGS=-static
