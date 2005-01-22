234364bcbd7a8f99be4ae7c78b72d795f16a43d4
bjoo <bjoo>
2005-01-22 12:59:42 +0000

    Fix in qdp_strnlen to account for odd situation on qcdoc

--- include/qdp_strnlen.h
+++ include/qdp_strnlen.h
@@ -3,15 +3,19 @@
 
 #include "qdp_config.h"
 
-#ifndef HAVE_STRNLEN
 #include <stdlib.h>
 #include <sys/types.h>
+#include <string.h>
 
+// Declare this. If it is already declared in string.h it should
+// still be ok.
+// On qcdoc it appears to have linkage but no prototype in which 
+// case it should also be declared
+// Likewise if a prototype were not to exist and no linkage were
+// to exist declaring it is still OK as qdp_strnlen.cc will provide
+// the linkage
 extern "C" { 
   size_t strnlen(const char *s, size_t maxlen);
 };
-#else
-#include <string.h>
-#endif
 
 #endif
