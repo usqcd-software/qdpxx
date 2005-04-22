af39756a2c3c84d3c20487ac594b050f09ffcc63
zbigniew <zbigniew>
2005-04-22 16:08:19 +0000

    A framework for doxygen-generated HTML documentation; User Guide and Reference Manual.

---
+++ docs/mkdocs.sh
@@ -0,0 +1,40 @@
+# $Id: mkdocs.sh,v 1.1 2005-04-22 16:08:19 zbigniew Exp $
+# Script to build the documentation
+
+# Allow this script to be run from its own directory or its parent directory.
+
+if [ `basename $PWD` = 'docs' ]
+then
+    docdir=`pwd -P`
+    topdir=`echo $docdir | sed -e 's|/docs$||'`
+else
+    topdir=`pwd -P` 
+    docdir=$topdir/docs
+fi
+
+# The version number
+# I assume that the tag is of the form <something>1-2-3<something>
+
+version=`echo '$Name:  $' | sed -e 's/[^1234567890-]//g' -e 'y/-/./'`
+
+tempdoxcfg=/tmp/dox$$
+
+for here in usr ref 
+do
+	sed -e "s/^PROJECT_NUMBER.*/PROJECT_NUMBER = $version/" $docdir/$here/doxygen.cfg > $tempdoxcfg
+	mv $tempdoxcfg $docdir/$here/doxygen.cfg
+done
+
+# Do any other doxygen config file editing here.
+
+
+# Run doxygen  
+
+for here in ref usr 
+do
+	cd $docdir/$here
+	doxygen doxygen.cfg		
+done
+
+
+
