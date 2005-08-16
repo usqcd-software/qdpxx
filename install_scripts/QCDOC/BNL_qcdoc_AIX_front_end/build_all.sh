4c6dd76a77ec5cfa82d04df61f3ce6f3665b79da
edwards <edwards>
2005-08-16 03:12:12 +0000

    Updated to use v2.6.0-CJ/aix5.2f

--- install_scripts/QCDOC/BNL_qcdoc_AIX_front_end/build_all.sh
+++ install_scripts/QCDOC/BNL_qcdoc_AIX_front_end/build_all.sh
@@ -4,16 +4,19 @@
 . ./build_functions.sh
 
 HERE=/home/edwardsr/qcd
-INSTALL_ROOT=/home/edwardsr/arch/v2.5.9
-QOS=/qcdoc/sfw/qos/v2.5.9/aix5.2
+INSTALL_ROOT=/home/edwardsr/arch/v2.6.0-CJ
+QOS=/qcdoc/sfw/qos/devel/v2.6.0-CJ/aix5.2f
 PRECISION=double
 HOST_SYS=powerpc-gnu-elf
 BUILD_SYS=none
+BAGEL_QDP=${HERE}/bagel_qdp
 BAGEL_INSTALL_DIR=${INSTALL_ROOT}/bagel
 BAGEL_CPU=ppc440
 BAGEL_ALLOC=qalloc
 BAGEL_COMM=qmp
 BAGEL_WILSON_DIR=${INSTALL_ROOT}/bagel_${PRECISION}_${BAGEL_CPU}_${BAGEL_ALLOC}_${BAGEL_COMM}
+BAGELQDP_INSTALLDIR=${INSTALL_ROOT}/bagel_qdp_${PRECISION}_${BAGEL_CPU}
+
 LIBXML_SRCDIR=${HERE}/libxml2-2.6.6
 LIBXML=${INSTALL_ROOT}/libxml2-2.6.6
 QDP_PARALLEL_ARCH=parscalar
@@ -55,27 +58,32 @@ fi
 ##
 
 ## Build BAGEL
-#build_bagel ${HERE}/bagel-1.3.2 ${BAGEL_INSTALL_DIR}
+build_bagel ${HERE}/bagel ${BAGEL_INSTALL_DIR}
 
 source ${QOS}/scripts/setup.sh
-export PATH=/home/edwardsr/bin/rs6000:$PATH
 ## Build Wilson Dslash
 
+build_bagel_wilson_dslash ${HERE}/bagel_wilson_dslash \
+                          ${BAGEL_WILSON_DIR} \
+		          ${BAGEL_INSTALL_DIR} \
+	                  ${PRECISION} ${BAGEL_COMM} ${BAGEL_ALLOC} \
+	                  ${BAGEL_CPU} ${HOST_SYS} ${BUILD_SYS} ${QOS}
 
-#build_bagel_wilson_dslash ${HERE}/bagel_wilson_dslash-1.3.2 \
-#                          ${BAGEL_WILSON_DIR} \
-#		          ${BAGEL_INSTALL_DIR} \
-#	                  ${PRECISION} ${BAGEL_COMM} ${BAGEL_ALLOC} \
-#	                  ${BAGEL_CPU} ${HOST_SYS} ${BUILD_SYS} ${QOS}
+build_bagel_qdp ${HERE}/bagel_qdp \
+                ${BAGEL_INSTALL_DIR} \
+		${BAGELQDP_INSTALLDIR} \
+                ${PRECISION} \
+                ${BAGEL_CPU} \
+                ${HOST_SYS} \
+		${BUILD_SYS}
 
-# build_libxml ${LIBXML_SRCDIR} ${LIBXML} ${HOST_SYS} ${BUILD_SYS}
+build_libxml ${LIBXML_SRCDIR} ${LIBXML} ${HOST_SYS} ${BUILD_SYS}
 
 ## Build QDP++
-#build_qdp  "${HERE}/qdp++" ${QDP_INSTALLDIR} ${QOS} ${LIBXML} ${PRECISION} ${QDP_DO_EDRAM} ${QDP_DO_BLAS} ${HOST_SYS} ${BUILD_SYS} ${BAGEL_WILSON_DIR}
-
+build_qdp  "${HERE}/qdp++" ${QDP_INSTALLDIR} ${QOS} ${LIBXML} ${PRECISION} ${QDP_DO_EDRAM} ${QDP_DO_BLAS} ${HOST_SYS} ${BUILD_SYS} ${BAGELQDP_INSTALLDIR}
 
 ## Build Chroma
-build_chroma ${HERE}/chroma ${CHROMA_INSTALLDIR} ${QDP_INSTALLDIR} ${HOST_SYS} ${BUILD_SYS} ${CHROMA_DO_PAB_DSLASH} ${CHROMA_DO_GMP} ${CHROMA_GMPDIR}
+build_chroma ${HERE}/chroma ${CHROMA_INSTALLDIR} ${QDP_INSTALLDIR} ${HOST_SYS} ${BUILD_SYS} ${CHROMA_DO_PAB_DSLASH} ${BAGEL_WILSON_DIR} ${CHROMA_DO_GMP} ${CHROMA_GMPDIR}
 
 pushd ${INSTALL_ROOT}
 find . -name "*" -type d -exec chmod ugo+rx {} \; -print
