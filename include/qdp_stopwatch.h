77b1211e7b0b91258beb529c30d6c3ea409e7ab8
edwards <edwards>
2005-10-17 04:27:36 +0000

    Moved inline codes to lib file.

--- include/qdp_stopwatch.h
+++ include/qdp_stopwatch.h
@@ -1,80 +1,49 @@
+// -*- C++ -*-
+// $Id: qdp_stopwatch.h,v 1.4 2005-10-17 04:27:36 edwards Exp $
+/*! @file
+ * @brief Timer support
+ *
+ * A stopwatch like timer.
+ */
+
+#include<sys/time.h>
+
 #ifndef QDP_STOPWATCH_H
 #define QDP_STOPWATCH_H
 
 QDP_BEGIN_NAMESPACE(QDP);
 
-#include<sys/time.h>
+/*! @defgroup timer Timer
+ *
+ * @ingroup qdp
+ *
+ * @{
+ */
+class StopWatch 
+{
+public:
+  //! Constructor
+  StopWatch();
 
-class StopWatch { 
- public:
-  StopWatch(void) {
-    stoppedP=false;
-    startedP=false;
+  //! Destructor
+  ~StopWatch();
 
-  }
+  //! Reset the timer
+  void reset();
 
-  ~StopWatch(void) {};
+  //! Start the timer
+  void start();
 
-  void reset(void) {
-    startedP = false;
-    stoppedP = false;
-  }
+  //! Stop the timer
+  void stop();
 
-  void start(void) {
-    int ret_val;
-    ret_val = gettimeofday(&t_start, NULL);
-    if( ret_val != 0 ) { 
-      QDP_error_exit("Gettimeofday failed in StopWatch::start()\n");
-    }
-    startedP = true;
-    stoppedP = false;
-  }
+  //! Get time in microseconds
+  double getTimeInMicroseconds();
 
-  void stop(void) {
-    if( !startedP ) { 
-      QDP_error_exit("Attempting to stop a non running stopwatch in StopWatch::stop()\n");
-    }
+  //! Get time in seconds
+  double getTimeInSeconds();
 
-    int ret_val;
-    ret_val = gettimeofday(&t_end, NULL);
-    if( ret_val != 0 ) {
-      QDP_error_exit("Gettimeofday failed in StopWatch::end()\n");
-    }
-    stoppedP = true;
-  }
-
-  double getTimeInMicroseconds(void) {
-    long usecs=0;
-    if( startedP && stoppedP ) { 
-      if( t_end.tv_sec < t_start.tv_sec ) { 
-	QDP_error_exit("Critical timer rollover\n");
-      }
-      else { 
-	usecs = (t_end.tv_sec - t_start.tv_sec)*1000000;
-
-
-	if( t_end.tv_usec < t_start.tv_usec ) {
-	  usecs -= 1000000;
-	  usecs += 1000000+t_end.tv_usec - t_start.tv_usec;
-	}
-	else {
-	  usecs += t_end.tv_usec - t_start.tv_usec;
-	}
-      }
-    }
-    else {
-      QDP_error_exit("Either stopwatch not started, or not stopped\n");
-    }
-
-    return (double)usecs;
-  }
-    
-  double getTimeInSeconds(void)  {
-    double t_sec = getTimeInMicroseconds() / 1e6;   
-    return t_sec;
-  }
-
- private:
+private:
   long sec;
   long usec;
   bool startedP;
