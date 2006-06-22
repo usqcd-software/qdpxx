5237e3239ebe1641c30c139114be83f359b9abc6
bjoo <bjoo>
2006-06-22 20:31:32 +0000

    Had to reenable the xmmintrin here to allow this to work.

--- include/qdp_sse_intrin.h
+++ include/qdp_sse_intrin.h
@@ -2,8 +2,7 @@
 #define QDP_SSE_INTRIN_H
 
 // Include the file with the SSE intrinsics  in it
-//#include <xmmintrin.h>
-#include <emmintrin.h> /* SSE2 */
+#include <xmmintrin.h>
 typedef __m128 v4sf;
 
 #endif 
