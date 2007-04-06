e79cb40ee9eae2d7102ab63707c0f478da0583a5
bjoo <bjoo>
2007-04-06 21:09:03 +0000

    OK Hopefully got rid of the make check build failure

--- examples/t_su3.cc
+++ examples/t_su3.cc
@@ -1,4 +1,4 @@
-// $Id: t_su3.cc,v 1.2 2007-04-05 20:54:43 bjoo Exp $
+// $Id: t_su3.cc,v 1.3 2007-04-06 21:09:03 bjoo Exp $
 
 #include <iostream>
 #include <cstdio>
@@ -16,18 +16,22 @@ int main(int argc, char *argv[])
   QDP_initialize(&argc, &argv);
 
   // Setup the layout
-  const int foo[] = {2,3,3,3};
+  const int foo[] = {2,4,6,8};
   multi1d<int> nrow(Nd);
   nrow = foo;  // Use only Nd elements
   Layout::setLattSize(nrow);
   Layout::create();
 
-#ifdef QDP_USE_BAGEL_QDP
+#if QDP_USE_BAGEL_QDP == 1
 
+  
   LatticeColorMatrix a, b, c;
   LatticeColorMatrix a2;
   LatticeColorMatrix diff;
 
+  // -----------------------------------------------------------------
+  // MM
+  // -----------------------------------------------------------------
   gaussian(b);
   gaussian(c);
 
@@ -65,6 +69,46 @@ int main(int argc, char *argv[])
 
   QDPIO::cout << "MM: original  seconds= " << original_secs << endl;
   QDPIO::cout << "MM: bagel_qdp seconds= " << qdp_secs << endl;
+
+
+  //-----------------------------------------------------------------
+  // MA
+  //-----------------------------------------------------------------
+  gaussian(b);
+  gaussian(c);
+
+  a = b*adj(c);
+  qdp_su3_ma(&(a2.elem(all.start()).elem().elem(0,0).real()),
+  	     &(b.elem(all.start()).elem().elem(0,0).real()),
+  	     &(c.elem(all.start()).elem().elem(0,0).real()),
+  	     num_sites);
+
+  diff = a - a2;
+
+  QDPIO::cout << "MA: || diff || = " << sqrt(norm2(diff)) << endl;
+
+  swatch.reset();
+  swatch.start();
+  for(int i=0; i < 5000; i++) { 
+    a = b*adj(c);
+  }
+  swatch.stop();
+  original_secs = swatch.getTimeInSeconds();
+
+  swatch.reset();
+  swatch.start();
+  for(int i=0; i < 5000; i++) {
+    qdp_su3_ma(&(a2.elem(all.start()).elem().elem(0,0).real()),
+	       &(b.elem(all.start()).elem().elem(0,0).real()),
+	       &(c.elem(all.start()).elem().elem(0,0).real()),
+	       num_sites);
+  }
+  swatch.stop();
+
+  qdp_secs = swatch.getTimeInSeconds();
+
+  QDPIO::cout << "MA: original  seconds= " << original_secs << endl;
+  QDPIO::cout << "MA: bagel_qdp seconds= " << qdp_secs << endl;
 
 #endif
 
