95bc44110ff928e009e06fc5447cfe674965110d
bjoo <bjoo>
2008-06-25 15:03:25 +0000

    Oopsie

--- include/scalarsite_sse/sse_linalg_mm_su3_double.h
+++ include/scalarsite_sse/sse_linalg_mm_su3_double.h
@@ -5,7 +5,7 @@
 namespace QDP { 
 
   /* M = a*M  a is scalar */
-  void ssed_m_eq_scal_m(REAL64* m2, REAL64* a, REAL *m1, int n_mat);
+  void ssed_m_eq_scal_m(REAL64* m2, REAL64* a, REAL64 *m1, int n_mat);
 
   /* M *= a,  a is a scalar */
   void ssed_m_muleq_scal(REAL64* m, REAL64* a, int n_mat);
@@ -24,39 +24,28 @@ namespace QDP {
 
   /* M3 = M1*M2 */
   void ssed_m_eq_mm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 = M1*adj(M2) */
+ 
+ /* M3 += a M1*M2 */
+  void ssed_m_peq_amm(REAL64* m3, REAL64* a, REAL64* m1, REAL64* m2, int n_mat); 
+
+  /* M3 = M1*adj(M2) */
   void ssed_m_eq_mh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
   
-/* M3 = adj(M1)*M2 */
+  /* M3 += a M1*adj(M2) */
+  void ssed_m_peq_amh(REAL64* m3, REAL64* a, REAL64* m1, REAL64* m2, int n_mat);
+
+  
+  /* M3 = adj(M1)*M2 */
   void ssed_m_eq_hm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 = adj(M1)*adj(M2) */
-  void ssed_m_eq_mm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
 
-  /* M3 += M1*M2 */
-  void ssed_m_peq_mm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
+  /* M3 += a adj(M1)*M2 */
+  void ssed_m_peq_ahm(REAL64* m3, REAL64*a, REAL64* m1, REAL64* m2, int n_mat);
   
-/* M3 += M1*adj(M2) */
-  void ssed_m_peq_mh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 += adj(M1)*M2 */
-  void ssed_m_peq_hm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 += adj(M1)*adj(M2) */
-  void ssed_m_peq_hh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
+  /* M3 = adj(M1)*adj(M2) */
+  void ssed_m_eq_hh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
 
-  /* M3 -= M1*M2 */
-  void ssed_m_meq_mm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 -= M1*adj(M2) */
-  void ssed_m_meq_mh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 -= adj(M1)*M2 */
-  void ssed_m_meq_hm(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
-  
-/* M3 -= adj(M1)*adj(M2) */
-  void ssed_m_meq_hh(REAL64* m3, REAL64* m1, REAL64* m2, int n_mat);
+  /* M3 += a adj(M1)*adj(M2) */
+  void ssed_m_peq_ahh(REAL64* m3, REAL64*a, REAL64* m1, REAL64* m2, int n_mat);
 
 
   
