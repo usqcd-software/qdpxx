6458e883d2b9b85848e7e328a6f3072aee30d27c
bjoo <bjoo>
2009-04-22 19:52:47 +0000

    Disabled spin aggregates in generics

--- include/qdp_scalarsite_generic.h
+++ include/qdp_scalarsite_generic.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: qdp_scalarsite_generic.h,v 1.14 2007-02-06 15:01:57 bjoo Exp $
+// $Id: qdp_scalarsite_generic.h,v 1.15 2009-04-22 19:52:47 bjoo Exp $
 
 /*! @file
  * @brief Generic optimizations
@@ -14,6 +14,6 @@
 #include "scalarsite_generic/qdp_scalarsite_generic_blas.h"
 #include "scalarsite_generic/qdp_scalarsite_generic_cblas.h"
 #include "scalarsite_generic/qdp_scalarsite_generic_blas_g5.h"
-#include "scalarsite_generic/generic_spin_aggregate.h"
+// #include "scalarsite_generic/generic_spin_aggregate.h"
 
 #endif
