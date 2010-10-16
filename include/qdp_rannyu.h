7216ed2fd39a33df5882dce6804a0b9d06cd207c
Robert Edwards <edwards@jlab.org>
2010-10-15 22:03:46 -0400

    Added some other variant of calls to rannyu.

--- include/qdp_rannyu.h
+++ include/qdp_rannyu.h
@@ -17,7 +17,7 @@ namespace QDP
 {
   namespace RANNYU
   {
-    //! The RNG
+    //! The RNG. Has side effects.
     double random();
 
     //! Seed has been set by default - this allows one to override it
@@ -25,6 +25,16 @@ namespace QDP
 
     //! Recover the seed
     multi1d<int> savern();
+
+    //! Hold state and RNG.
+    struct RNGState_t
+    {
+      multi1d<int> seed;   /*!< The state of the RNG. */
+      double       ran;    /*!< Output of the RNG. */
+    };
+
+    //! The RNG, but packaging all the state with it. No side effects.
+    void random(RNGState_t& ran_state);
 
   } // namespace RANNYU
 
--- lib/qdp_rannyu.cc
+++ lib/qdp_rannyu.cc
@@ -23,26 +23,55 @@ namespace QDP
     namespace
     {
       int m[4] = {0, 1, 3513, 821};
-      int l[4] = {13, 11, 5, 1};
+
+      multi1d<int> ran_seed;
+      int __default_seed[4] = {13, 15, 1, 17};
+      bool inited = false;
+      
+      double twom12 = 1/4096.0;
     }
 
+    // Hide the actual RNG
+    namespace
+    {
+      void __rand(double& ran, multi1d<int>& ll)
+      {
+	int ii[4];
+
+	ii[0] = ll[0]*m[3] + ll[1]*m[2] + ll[2]*m[1] + ll[3]*m[0];
+	ii[1] = ll[1]*m[3] + ll[2]*m[2] + ll[3]*m[1];
+	ii[2] = ll[2]*m[3] + ll[3]*m[2];
+	ii[3] = ll[3]*m[3] + 1;
+	ll[3] = ii[3] & 4095;
+	ii[2] = ii[2] + (ii[3] >> 12);
+	ll[2] = ii[2] & 4095;
+	ii[1] = ii[1] + (ii[2] >> 12);
+	ll[1] = ii[1] & 4095;
+	ll[0] = (ii[0] + (ii[1] >> 12)) >> 12;
+	ran = twom12*((double)ll[0] + twom12*((double)ll[1] + twom12*((double)ll[2] + twom12*((double)ll[3]))));
+      }
+    }
+
+
+    //! The RNG. Has side effects.
     double random()
     {
-      double twom12 = 1/4096.0;
-      int i[4];
+      double ran;
 
-      i[0] = l[0]*m[3] + l[1]*m[2] + l[2]*m[1] + l[3]*m[0];
-      i[1] = l[1]*m[3] + l[2]*m[2] + l[3]*m[1];
-      i[2] = l[2]*m[3] + l[3]*m[2];
-      i[3] = l[3]*m[3] + 1;
-      l[3] = i[3] & 4095;
-      i[2] = i[2] + (i[3] >> 12);
-      l[2] = i[2] & 4095;
-      i[1] = i[1] + (i[2] >> 12);
-      l[1] = i[1] & 4095;
-      l[0] = (i[0] + (i[1] >> 12)) >> 12;
-      return twom12*((double)l[0] + twom12*((double)l[1] + twom12*((double)l[2] + twom12*((double)l[3]))));
+      // Initialize the global seed if needed
+      if (! inited)
+      {
+	ran_seed.resize(4);
+	for(int i=0; i < 4; ++i)
+	  ran_seed[i] = __default_seed[i];
+
+	inited = true;
+      }
+
+      __rand(ran, ran_seed);
+      return ran;
     }
+
 
     //! Seed has been set by default - this allows one to override it
     void setrn(const multi1d<int>& iseed)
@@ -53,19 +82,27 @@ namespace QDP
 	QDP_abort(1);
       }
 	
-      for(int i=0; i < 4; ++i)
-	l[i] = iseed[i];
+      ran_seed = iseed;
     }
+
 
     //! Recover the seed
     multi1d<int> savern()
     {
-      multi1d<int> iseed(4);
+      return ran_seed;
+    }
 
-      for(int i=0; i < 4; ++i)
-	iseed[i] = l[i];
 
-      return iseed;
+    // The RNG, but packaging all the state with it. No side effects.
+    void random(RNGState_t& ran_state)
+    {
+      if (ran_state.seed.size() != 4)
+      {
+	QDPIO::cerr << __func__ << ": rannyu seed is not length 4\n";
+	QDP_abort(1);
+      }
+	
+      __rand(ran_state.ran, ran_state.seed);
     }
 
   } // namespace RANNYU
