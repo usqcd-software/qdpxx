135713b546d62beae6c32d87dd94720c242ab0a5
Frank Winter <frank.winter@ed.ac.uk>
2012-08-29 11:30:28 +0100

    Completes eval inner leaf

--- include/qdp_newops.h
+++ include/qdp_newops.h
@@ -712,6 +712,14 @@ struct LeafFunctor<GammaType<N>, EvalLeaf1>
     {return a;}
 };
 
+template<int N>
+struct LeafFunctor<GammaType<N>, EvalInnerLeaf1>
+{
+  typedef GammaType<N> Type_t;
+  inline static Type_t apply(const GammaType<N> &a, const EvalInnerLeaf1 &f)
+    {return a;}
+};
+
 
 template<int N, int m>
 struct CreateLeaf<GammaConst<N,m> >
@@ -736,6 +744,14 @@ struct LeafFunctor<GammaConst<N,m>, EvalLeaf1>
 {
   typedef GammaConst<N,m> Type_t;
   inline static Type_t apply(const GammaConst<N,m> &a, const EvalLeaf1 &f)
+    {return a;}
+};
+
+template<int N, int m>
+struct LeafFunctor<GammaConst<N,m>, EvalInnerLeaf1>
+{
+  typedef GammaConst<N,m> Type_t;
+  inline static Type_t apply(const GammaConst<N,m> &a, const EvalInnerLeaf1 &f)
     {return a;}
 };
 
@@ -772,6 +788,14 @@ struct LeafFunctor<GammaTypeDP<N>, EvalLeaf1>
     {return a;}
 };
 
+template<int N>
+struct LeafFunctor<GammaTypeDP<N>, EvalInnerLeaf1>
+{
+  typedef GammaTypeDP<N> Type_t;
+  inline static Type_t apply(const GammaTypeDP<N> &a, const EvalInnerLeaf1 &f)
+    {return a;}
+};
+
 
 template<int N, int m>
 struct CreateLeaf<GammaConstDP<N,m> >
@@ -796,6 +820,14 @@ struct LeafFunctor<GammaConstDP<N,m>, EvalLeaf1>
 {
   typedef GammaConstDP<N,m> Type_t;
   inline static Type_t apply(const GammaConstDP<N,m> &a, const EvalLeaf1 &f)
+    {return a;}
+};
+
+template<int N, int m>
+struct LeafFunctor<GammaConstDP<N,m>, EvalInnerLeaf1>
+{
+  typedef GammaConstDP<N,m> Type_t;
+  inline static Type_t apply(const GammaConstDP<N,m> &a, const EvalInnerLeaf1 &f)
     {return a;}
 };
 
