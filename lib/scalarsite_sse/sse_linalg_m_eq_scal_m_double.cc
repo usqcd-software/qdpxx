eeea76b85a38a3053c0b864435da0c0ccff0d3ef
Balint Joo <bjoo@jlab.org>
2013-01-18 10:11:01 -0500

    Oops. Typo fix

--- lib/scalarsite_sse/sse_linalg_m_eq_scal_m_double.cc
+++ lib/scalarsite_sse/sse_linalg_m_eq_scal_m_double.cc
@@ -112,7 +112,7 @@ namespace QDP {
     
     // cross components into tmp 
     // Zero tmp
-    tmp1 = _mm_setzero_pd():
+    tmp1 = _mm_setzero_pd();
     tmp1 = _mm_shuffle_pd(scalar, scalar, 0x1);
     scalar = _mm_add_pd(scalar, tmp1);
 
