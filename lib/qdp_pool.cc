57a39437a1e3edfeb8e24b35597d1448353f5322
Jacques Bloch <jacques.bloch@ur.de>
2014-01-30 11:27:52 +0100

    Modified implementation of Pool to use a Pool class. Added compiler option to use Pool for all lattice objects or only for Fermions and ColorMatrices

--- lib/qdp_pool.cc
+++ lib/qdp_pool.cc
@@ -1,86 +1,125 @@
 // Jacques Bloch - 24 Jan 2014
+/*
+	Pool to allocate lattice objects
+*/
+
 #include <vector>
 #include "qdp.h"
 #include "qdp_pool.h"
+//#define LARGE_POOL
 
 namespace QDP
 {
 	namespace Pool
 	{
-		static list<void*> avail[2], used[2];
+		static int VERBOSE=0;
 
-		void *allocate(size_t n_bytes,const QDP::Allocator::MemoryPoolHint& mem_pool_hint)
+#ifdef LARGEPOOL
+		int Npool = 7;
+		Pool pool[]=
 		{
-			bool usepool;
-			int ipool;
+			Pool("Fermion", sizeof(Fermion)),
+			Pool("ColorMatrix", sizeof(ColorMatrix)),
+			Pool("HalfFermion", sizeof(HalfFermion)),
+			Pool("Complex", sizeof(RComplex<REAL>)),
+			Pool("Real", sizeof(REAL)),
+			Pool("int", sizeof(int)),
+			Pool("byte", sizeof(char))
+		};
+#else
+		int Npool = 2;
+		Pool pool[]=
+		{
+			Pool("Fermion", sizeof(Fermion)),
+			Pool("ColorMatrix", sizeof(ColorMatrix)),
+		};
+#endif
 
-			if (n_bytes == Layout::sitesOnNode()*Ns*Nc*2*sizeof(REAL))	// LatticeFermion
+#ifdef ORIGINALPOOL
+		#warning "USING ORIGINAL POOL"
+
+		// constructor
+		Pool::Pool(const string name, size_t n_bytes) : name(name), n_bytes(n_bytes) {}
+
+		// destructor
+		Pool::~Pool()
+		{
+				QDPIO::cout << "pool " << name << " released: total=" << avail.size()+used.size() << " free=" << avail.size() << " used=" << used.size() << endl;
+		}
+
+		void *Pool::allocate(size_t n_bytes,const QDP::Allocator::MemoryPoolHint& mem_pool_hint)
+		{
+			if(avail.empty())
 			{
-				usepool=true;
-				ipool=0;
-			}
-			else if (n_bytes == Layout::sitesOnNode()*Nc*Nc*2*sizeof(REAL))	// LatticeColorMatrix
-			{
-				usepool=true;
-				ipool=1;
+				used.push_back(QDP::Allocator::theQDPAllocator::Instance().allocate(n_bytes,mem_pool_hint));
 			}
 			else
-				usepool=false;
-
-			if (!usepool)
 			{
-					return QDP::Allocator::theQDPAllocator::Instance().allocate(n_bytes,mem_pool_hint);
+					used.push_back(avail.back());
+					avail.pop_back();
+			}
+			if (VERBOSE) cout << "pool " << name << " : total=" << avail.size()+used.size() << " free=" << avail.size() << " used=" << used.size() << endl;
+			return used.back();
+		}
+
+		void Pool::free(void *ptr)
+		{
+			Memcontainer::iterator iter=find(used.begin(), used.end(),ptr);
+			if (iter != used.end())
+			{
+				used.erase(iter);
+				avail.push_back(ptr);
+				if (VERBOSE) cout << "pool " << name << " : total=" << avail.size()+used.size() << " free=" << avail.size() << " used=" << used.size() << endl;
+			}
+			else
+				QDPIO::cout << "Object is not allocated!\n";
+		}
+#else
+
+		#warning "USING NEW POOL"
+
+		// constructor
+		Pool::Pool(const string name, size_t n_bytes) : avail(0), n_bytes(n_bytes), name(name) {}
+
+		// destructor
+		Pool::~Pool()
+		{
+				QDPIO::cout << "pool " << name << " released: total=" << ptr.size() << " free=" << avail << " used=" << ptr.size()-avail << endl;
+		}
+
+		void *Pool::allocate(size_t n_bytes,const QDP::Allocator::MemoryPoolHint& mem_pool_hint)
+		{
+			if(!avail)
+			{
+				ptr.push_back(QDP::Allocator::theQDPAllocator::Instance().allocate(n_bytes,mem_pool_hint));
+				flag_used.push_back(true);
+				if (VERBOSE) cout << "pool " << name << ": total=" << ptr.size() << " free=" << avail << " used=" << ptr.size()-avail << endl;
+				return ptr.back();
 			}
 			else
 			{
-				if(avail[ipool].empty())
-				{
-		used[ipool].push_back(QDP::Allocator::theQDPAllocator::Instance().allocate(n_bytes,mem_pool_hint));
-				}
-				else
-				{
-						used[ipool].push_back(avail[ipool].back());
-						avail[ipool].pop_back();
-				}
-				cout << "pool " << ipool << " : total=" << avail[ipool].size()+used[ipool].size() << " free=" << avail[ipool].size() << " used=" << used[ipool].size() << endl;
-				return used[ipool].back();
+				// search for unused element
+				vector<bool>::iterator iter=find(flag_used.begin(), flag_used.end(), false);
+				*iter = true;
+				--avail;
+				if (VERBOSE) cout << "pool " << name << ": total=" << ptr.size() << " free=" << avail << " used=" << ptr.size()-avail << endl;
+				return ptr[iter-flag_used.begin()];
 			}
 		}
 
-		void free(void *ptr, size_t n_bytes)
+		void Pool::free(void *mem)
 		{
-			bool usepool;
-			int ipool;
-
-			if (n_bytes == Layout::sitesOnNode()*Ns*Nc*2*sizeof(REAL))	// LatticeFermion
+			vector<void*>::iterator iter=find(ptr.begin(), ptr.end(),mem);
+			if (iter != ptr.end())
 			{
-				usepool=true;
-				ipool=0;
-			}
-			else if (n_bytes == Layout::sitesOnNode()*Nc*Nc*2*sizeof(REAL))	// LatticeColorMatrix
-			{
-				usepool=true;
-				ipool=1;
+				flag_used[iter-ptr.begin()] = false;
+				++avail;
+				if (VERBOSE) cout << "pool " << name << ": total=" << ptr.size() << " free=" << avail << " used=" << ptr.size()-avail << endl;
 			}
 			else
-				usepool=false;
-
-			if (!usepool)
-			{
-				QDP::Allocator::theQDPAllocator::Instance().free(ptr);
-			}
-			else
-			{
-				list<void*>::iterator iter=find(used[ipool].begin(), used[ipool].end(),ptr);
-				if (iter != used[ipool].end())
-				{
-					used[ipool].erase(iter);
-					avail[ipool].push_back(ptr);
-					cout << "pool " << ipool << " : total=" << avail[ipool].size()+used[ipool].size() << " free=" << avail[ipool].size() << " used=" << used[ipool].size() << endl;
-				}
-				else
-					QDPIO::cout << "Object is not allocated!\n";
-			}
+				QDPIO::cout << "Object is not allocated!\n";
 		}
+#endif
+
 	}		// namespace Pool
 }		// namespace QDP
