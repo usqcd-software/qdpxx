ec8053987b3d816ff9a0f890acdae3f82ddbc7b6
Robert Edwards <edwards@jlab.org>
2014-03-30 17:21:38 -0400

    Removed more of those obsolete "register" qualifiers.

--- lib/scalarsite_sse/sse_blas_local_vcdot_double.cc
+++ lib/scalarsite_sse/sse_blas_local_vcdot_double.cc
@@ -95,10 +95,10 @@ namespace QDP {
   void local_vcdot4(REAL64 *sum, REAL64 *y, REAL64* x,int n_4spin)
 {
   // Use _mm_setzero_pd() to initialize the sums rather than xors
-  register __m128d sum1 = _mm_setzero_pd();
-  register __m128d sum2 = _mm_setzero_pd();
-  register __m128d sum3 = _mm_setzero_pd();
-  register __m128d sum4 = _mm_setzero_pd();
+  __m128d sum1 = _mm_setzero_pd();
+  __m128d sum2 = _mm_setzero_pd();
+  __m128d sum3 = _mm_setzero_pd();
+  __m128d sum4 = _mm_setzero_pd();
 
   __m128d tmp1;
   __m128d tmp2;
