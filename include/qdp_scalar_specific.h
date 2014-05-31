cb117d224e56399c5be2e24abb32bc27ffec78e4
Robert Edwards <edwards@jlab.org>
2014-05-30 23:42:06 -0400

    Added evaluate_F to qdp_scalar_specific.

--- include/qdp_scalar_specific.h
+++ include/qdp_scalar_specific.h
@@ -233,6 +233,46 @@ void evaluate(OLattice<T>& dest, const Op& op, const QDPExpr<RHS,OLattice<T1> >&
 
 
 //-----------------------------------------------------------------------------
+template<class T, class T1, class Op, class RHS>
+//inline
+void evaluate_F(T* dest, const Op& op, const QDPExpr<RHS,OLattice<T1> >& rhs,
+	      const Subset& s)
+{
+  //cerr << "In evaluate_F(olattice,olattice)" << endl;
+
+#if defined(QDP_USE_PROFILING)   
+  static QDPProfile_t prof(dest, op, rhs);
+  prof.time -= getClockTime();
+#endif
+
+  // int numSiteTable = s.numSiteTable();
+  // user_arg<T,T1,Op,RHS> a(dest, rhs, op, s.siteTable().slice());
+  // dispatch_to_threads< user_arg<T,T1,Op,RHS> >(numSiteTable, a, evaluate_userfunc);
+
+  ////////////////////
+  // Original code
+  ///////////////////
+
+  //QDP_info("eval_F %d sites",s.numSiteTable());
+
+  // General form of loop structure
+  const int *tab = s.siteTable().slice();
+  for(int j=0; j < s.numSiteTable(); ++j) 
+  {
+    int i = tab[j];
+    //fprintf(stderr,"eval(olattice,olattice): site %d\n",i);
+    op( dest[j], forEach(rhs, EvalLeaf1(i), OpCombine()));
+  }
+
+#if defined(QDP_USE_PROFILING)   
+  prof.time += getClockTime();
+  prof.count++;
+  prof.print();
+#endif
+}
+
+
+//-----------------------------------------------------------------------------
 //! dest = (mask) ? s1 : dest
 template<class T1, class T2> 
 void 
