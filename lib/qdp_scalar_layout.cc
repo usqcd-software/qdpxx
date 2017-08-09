376213b2d4ae6d935fde2798de92d32e1d52b256
Balint Joo <bjoo@jlab.org>
2017-08-09 18:26:02 -0400

    Added TBB allocator to scalar branch of code.

--- lib/qdp_scalar_layout.cc
+++ lib/qdp_scalar_layout.cc
@@ -16,10 +16,11 @@
 
 #include "qdp.h"
 #include "qdp_util.h"
-
+#include "qdp_allocator.h"
 
 namespace QDP 
 {
+  float pool_size_in_gb = 8.0;
 
   //-----------------------------------------------------------------------------
   // Layout stuff specific to a scalar architecture
@@ -260,7 +261,9 @@ namespace QDP
 	if (i != j)
 	  QDP_error_exit("Layout::create - Layout problems, the layout functions do not work correctly with this lattice size");
       }
+     size_t pool_size_in_MB = static_cast<size_t>(floor(pool_size_in_gb*1024.0));
 
+      Allocator::theQDPAllocator::Instance().init(pool_size_in_MB);
       // Initialize various defaults
       initDefaults();
 
