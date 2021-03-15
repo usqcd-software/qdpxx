47a8a50ec78f2ef4e3617eff90152541a6981104
Eloy Romero <eromero@jlab.org>
2021-03-15 10:49:29 -0400

    Replace byte_swap by a version without alignment requirements

--- lib/qdp_byteorder.cc
+++ lib/qdp_byteorder.cc
@@ -16,159 +16,43 @@ namespace QDPUtil
   //! Is the native byte order big endian?
   bool big_endian()
   {
-    union {
-      int  l;
-      char c[sizeof(int)];
-    } u;
-    u.l = 1;
-    return (u.c[sizeof(int) - 1] == 1);
+    int i = 1;
+    return (((char*)&i)[sizeof(int) - 1] == 1);
   }
 
-
   //! Byte-swap an array of data each of size nmemb
-  void byte_swap(void *ptr, size_t size, size_t nmemb)
+  // NOTE: p has no special alignment requirement
+  template <size_t size>
+  static void byte_swap(char* p, size_t nmemb)
   {
-    unsigned int j;
+    // Quick exit
+    if (size <= 1)
+      return;
 
-    char char_in[16];		/* characters used in byte swapping */
+    // Change the endianness
+    for (size_t i = 0; i < nmemb; i++)
+      for (size_t j = 0; j < size / 2; j++)
+	std::swap(p[i * size + j], p[i * size + size - 1 - j]);
+  }
 
-    char *in_ptr;
-    double *double_ptr;		/* Pointer used in the double routines */
-
-    switch (size)
+  void byte_swap(void* ptr, size_t size, size_t nmemb)
+  {
+    if (size == 1)
+      byte_swap<1>((char*)ptr, nmemb);
+    else if (size == 2)
+      byte_swap<2>((char*)ptr, nmemb);
+    else if (size == 4)
+      byte_swap<4>((char*)ptr, nmemb);
+    else if (size == 8)
+      byte_swap<8>((char*)ptr, nmemb);
+    else if (size == 16)
+      byte_swap<16>((char*)ptr, nmemb);
+    else
     {
-    case 4:  /* n_uint32_t */
-    {
-      n_uint32_t *w = (n_uint32_t *)ptr;
-      n_uint32_t old, recent;
-
-      for(j=0; j<nmemb; j++)
-      {
-	old = w[j];
-	recent = old >> 24 & 0x000000ff;
-	recent |= old >> 8 & 0x0000ff00;
-	recent |= old << 8 & 0x00ff0000;
-	recent |= old << 24 & 0xff000000;
-	w[j] = recent;
-      }
-    }
-    break;
-
-    case 1:  /* n_uint8_t: byte - do nothing */
-      break;
-
-    case 8:  /* n_uint64_t */
-    {
-      for(j = 0, double_ptr = (double *) ptr;
-	  j < nmemb;
-	  j++, double_ptr++)
-      {
-	in_ptr = (char *) double_ptr; /* Set the character pointer to
-					 point to the start of the double */
-
-	/*
-	 *  Assign all the byte variables to a character
-	 */
-	char_in[0] = in_ptr[0];
-	char_in[1] = in_ptr[1];
-	char_in[2] = in_ptr[2];
-	char_in[3] = in_ptr[3];
-	char_in[4] = in_ptr[4];
-	char_in[5] = in_ptr[5];
-	char_in[6] = in_ptr[6];
-	char_in[7] = in_ptr[7];
-
-	/*
-	 *  Now just swap the order
-	 */
-	in_ptr[0] = char_in[7];
-	in_ptr[1] = char_in[6];
-	in_ptr[2] = char_in[5];
-	in_ptr[3] = char_in[4];
-	in_ptr[4] = char_in[3];
-	in_ptr[5] = char_in[2];
-	in_ptr[6] = char_in[1];
-	in_ptr[7] = char_in[0];
-      }
-    }
-    break;
-
-    case 16:  /* Long Long */
-    {
-      for(j = 0, double_ptr = (double *) ptr;
-	  j < nmemb;
-	  j++, double_ptr+=2)
-      {
-
-	in_ptr = (char *) double_ptr; /* Set the character pointer to
-					 point to the start of the double */
-
-	/*
-	 *  Assign all the byte variables to a character
-	 */
-	char_in[0] = in_ptr[0];
-	char_in[1] = in_ptr[1];
-	char_in[2] = in_ptr[2];
-	char_in[3] = in_ptr[3];
-	char_in[4] = in_ptr[4];
-	char_in[5] = in_ptr[5];
-	char_in[6] = in_ptr[6];
-	char_in[7] = in_ptr[7];
-	char_in[8] = in_ptr[8];
-	char_in[9] = in_ptr[9];
-	char_in[10] = in_ptr[10];
-	char_in[11] = in_ptr[11];
-	char_in[12] = in_ptr[12];
-	char_in[13] = in_ptr[13];
-	char_in[14] = in_ptr[14];
-	char_in[15] = in_ptr[15];
-
-	/*
-	 *  Now just swap the order
-	 */
-	in_ptr[0] = char_in[15];
-	in_ptr[1] = char_in[14];
-	in_ptr[2] = char_in[13];
-	in_ptr[3] = char_in[12];
-	in_ptr[4] = char_in[11];
-	in_ptr[5] = char_in[10];
-	in_ptr[6] = char_in[9];
-	in_ptr[7] = char_in[8];
-
-	in_ptr[8] = char_in[7];
-	in_ptr[9] = char_in[6];
-	in_ptr[10] = char_in[5];
-	in_ptr[11] = char_in[4];
-	in_ptr[12] = char_in[3];
-	in_ptr[13] = char_in[2];
-	in_ptr[14] = char_in[1];
-	in_ptr[15] = char_in[0];
-
-      }
-    }
-    break;
-
-    case 2:  /* n_uint16_t */
-    {
-      n_uint16_t *w = (n_uint16_t *)ptr;
-      n_uint16_t old, recent;
-
-      for(j=0; j<nmemb; j++)
-      {
-	old = w[j];
-	recent = old >> 8 & 0x00ff;
-	recent |= old << 8 & 0xff00;
-	w[j] = recent;
-      }
-    }
-    break;
-
-    default:
       std::cerr << __func__ << ": unsupported word size = " << size << "\n";
       exit(1);
     }
   }
-
 
   //! fread on a binary file written in big-endian order
   size_t bfread(void *ptr, size_t size, size_t nmemb, FILE *stream)
